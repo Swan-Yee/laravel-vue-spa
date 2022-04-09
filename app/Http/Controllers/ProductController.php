634c450d3d5261645c9629ce26363fc88f180216
swanyee <swanyeetun.2000@gmail.com>
2022-04-09 21:12:16 +0630

    vue intalled

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request)
+    {
+        $product = Product::create($request->only('name','price'));
+        return $product;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductRequest $request, Product $product)
+    {
+        $product->update($request->only('name','price'));
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return $product;
+    }
+}
---
+++ resources/js/components/ProductComponent.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container my-5">
+        <div class="row">
+            <div class="row">
+                <div class="col-8 offset-4 d-flex justify-content-between">
+                    <button class="btn btn-primary">Create</button>
+                    <form>
+                        <div class="input-group">
+                            <input type="text" class="form-control" />
+                            <div class="input-group-append">
+                                <button type="submit" class="btn btn-primary">
+                                    Search
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <h3 class="card-header">Create</h3>
+                    <div class="card-body">
+                        <form action="">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" class="form-control" />
+                            </div>
+                            <div class="form-group">
+                                <label>Price</label>
+                                <input type="number" class="form-control" />
+                            </div>
+                            <div class="mt-4">
+                                <div class="btn btn-primary">Save</div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-8">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Book</td>
+                            <td>1000</td>
+                            <td>
+                                <button class="btn btn-success">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
